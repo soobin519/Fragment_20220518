ae20ea108aea02c1249ca2a3cc02c958902a1630
soobin519 <soobin0519@naver.com>
2022-05-18 11:24:55 +0900

    xml fragment 직접 배치하기

--- app/src/main/java/kr/co/asianaidt/fragment_20220518/FirstFragment.kt
+++ app/src/main/java/kr/co/asianaidt/fragment_20220518/FirstFragment.kt
@@ -1,10 +1,12 @@
 package kr.co.asianaidt.fragment_20220518
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 class FirstFragment : Fragment(){
@@ -16,6 +18,17 @@ class FirstFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate( R.layout.fragment_first, container, false )
+    }
+
+    //동작 관련 코드 전달 함수
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLog.setOnClickListener {
+
+            Log.d("첫 프래그먼트", "프래그먼트에서의 로그")
+
+        }
     }
 
 }
